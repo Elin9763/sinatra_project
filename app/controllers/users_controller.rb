e305dda612b8205c698ae72391a4cc7d483bdba4
Eric Lin <esotericlin@gmail.com>
2018-03-28 00:26:54 +0000

    continuing login/signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,33 @@ class UsersController < ApplicationController
     end
   end
 
+  post '/users/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/recipes'
+    else
+      session[:error] = "Incorrect"
+      redirect to '/login'
+    end
+  end
+
   get '/signup' do
     erb :'users/signup'
   end
 
+  post '/users/signup' do
+    @user = User.new(username: params[:username], password: params[:password], email: params[:email])
+    if @user.save
+      redirect '/recipes'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/users/show'
+  end
 
 end
