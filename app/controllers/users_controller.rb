c31c3a3b539a6fcacfa033be7d10f45ab37a4e61
Eric Lin <esotericlin@gmail.com>
2018-04-04 22:25:55 +0000

    revert pt.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     end
   end
 
-  post '/users/login' do
+  post '/login' do
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
     erb :'users/signup'
   end
 
-  post '/users/signup' do
+  post '/signup' do
     @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     if @user.save
       session[:user_id] = @user.id
